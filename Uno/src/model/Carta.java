a24ab3681c80386a57f689cca571d71481708c72
DenisMagno <denis_magno16@hotmail.com>
2017-11-14 19:57:07 -0200

    Atualização da classe 'Carta' #1

--- Uno/src/model/Carta.java
+++ Uno/src/model/Carta.java
@@ -2,6 +2,7 @@ package model;
 
 /*
     CONVENÇÃO
+
 Cor pode ser: amarelo(1), azul(2), verde(3), vermelho(4) e preta/coringa(5)
 Cores numeradas pode ser: 1 a 9, 10(bloqueio), 11(inverte), 12(mais2)
 Sendo duas para cada cor e 0 uma para cada cor
