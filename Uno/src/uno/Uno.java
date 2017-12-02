c9e3db66b54be02fd76ea8e77baddc9ca0261b3e
DenisMagno <denis_magno16@hotmail.com>
2017-12-02 19:06:07 -0200

    Relação entre view e jogo #2

--- Uno/src/uno/Uno.java
+++ Uno/src/uno/Uno.java
@@ -1,15 +1,10 @@
 package uno;
 
-import model.*;
+import view.InicioView;
 
 public class Uno {
     public static void main(String[] args) {
-        Jogo jogo = new Jogo();
-        jogo.iniciaJogo("Denis", "Mariana");
-        
-        System.out.println("Quantidade na pilha de compras: "+ jogo.monteCompra.getListaCartas().size());
-        System.out.println("Quantidade na pilha de descarte: "+ jogo.pilhaDescarte.getListaCartas().size());
-        System.out.println("Quantidade na mão do jogador Denis: "+ jogo.jogador1.getMao().getListaCartas().size());
-        System.out.println("Quantidade na mão do jogador Mariana: "+ jogo.jogador2.getMao().getListaCartas().size());
+        InicioView view = new InicioView();
+        view.setVisible(true);
     }
 }
